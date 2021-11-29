f0101a0b0d9a9c027625502618a51f84c7cf66b9
olimarie21 <olivia.underdah@gmail.com>
2021-11-29 13:03:22 -0800

    custom loop edits

--- template-parts/content-design.php
+++ template-parts/content-design.php
@@ -7,15 +7,17 @@
       <?php the_post_thumbnail( );?></a>
   </div>
 
-  <h3 class="project-title">
-      <a href="<?php the_permalink();?>"><?php the_title();?></a>
-  </h3>
+  <div class="project-details">
+    <h3 class="project-title">
+        <a href="<?php the_permalink();?>"><?php the_title();?></a>
+    </h3>
+
+    <h4 class="project-tools">
+      <?php the_meta() ;?>
+    </h4>
+  </div>
+
 </article>
 
 
 
-
-
-  <!-- <div class="author-desc">
-    <?php the_field('author_desc');?>
-  </div>  -->
