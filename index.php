3f756f1afff991e3b6b9ef7326fcef98db2d53ab
olimarie21 <olivia.underdah@gmail.com>
2021-12-03 08:38:09 -0800

    remove image from error page

--- index.php
+++ index.php
@@ -9,7 +9,6 @@ if ( have_posts() ) :
   endwhile;
   elseif (is_404(  )) :?>
     <h2 class="error-sceen">Oops...the page doesn't exist</h2>
-    <img class="error-image" src="http://localhost/assignment7duplicator_package/wp-content/uploads/2021/06/jelleke-vanooteghem-kabtmcdcAbk-unsplash.jpg">
   <?php else :
     get_template_part('template-parts/content-none.php');
 endif;  
